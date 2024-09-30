e9baa143e8906676e05272061f1ec37a5df87e79
Eunsoo Justin Shin <shinej@uchicago.edu>
2024-09-30 08:32:06 -0400

    g

---
+++ hw2/p3/lptest.py
@@ -0,0 +1,30 @@
+import cvxpy as cp
+import numpy as np
+import barrier
+
+
+
+if __name__ == "__main__":
+    #the optimization function c:
+    c = np.array([2, 1])
+
+    #pick a starting point (this can be done autonomously but we'll do it by hand)
+    x = cp.Variable(2)
+
+    hyperplanes = np.array(
+        [[0.7071,    0.7071, 1.5], 
+        [-0.7071,    0.7071, 1.5],
+        [0.7071,    -0.7071, 1],
+        [-0.7071,    -0.7071, 1]]
+    )
+
+    #let's break down the data into variables we are familiar with
+    a = hyperplanes[:,:2] # each column is the "a" part of a hyperplane
+    b = hyperplanes[:,2] # each row is the "b" part of a hyperplane (only one element in each row)
+
+    constraints = [a @ x <= b]
+    objective = cp.Minimize(c.T @ x)
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    print("The optimal point: (%f, %f)" % (x.value[0], x.value[1]))
+
