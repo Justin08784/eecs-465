9f4d978b6adf10b1705881055e16896add75d23e
Justin Shin <shine777justin@gmail.com>
2024-10-19 18:53:27 -0400

    moving code to YOUR CODE section

--- hw3/astar_template.py
+++ hw3/astar_template.py
@@ -11,6 +11,31 @@ import itertools
 
 import itertools
 def main(screenshot=False):
+    # initialize PyBullet
+    connect(use_gui=True)
+    # load robot and obstacle resources
+    robots, obstacles = load_env('pr2doorway.json')
+
+    # define active DoFs
+    base_joints = [joint_from_name(robots['pr2'], name) for name in PR2_GROUPS['base']]
+
+    collision_fn = get_collision_fn_PR2(robots['pr2'], base_joints, list(obstacles.values()))
+    # Example use of collision checking
+    # print("Robot colliding? ", collision_fn((0.5, -1.3, -np.pi/2)))
+
+    # Example use of setting body poses
+    # set_pose(obstacles['ikeatable6'], ((0, 0, 0), (1, 0, 0, 0)))
+
+    # Example of draw 
+    # draw_sphere_marker((0, 0, 1), 0.1, (1, 0, 0, 1))
+    
+    start_config = tuple(get_joint_positions(robots['pr2'], base_joints))
+    goal_config = (2.6, -1.3, -np.pi/2)
+    path = []
+
+    start_time = time.time()
+    ### YOUR CODE HERE ###
+    
     # grid details
     xlimit = 4.1 # x = [-xlimit, xlimit]
     ylimit = 2.1 # y = [-ylimit, ylimit]
@@ -30,24 +55,6 @@ def main(screenshot=False):
                -ylimit + lin_res*iy,\
                ang_res*ir
 
-
-    # initialize PyBullet
-    connect(use_gui=True)
-    # load robot and obstacle resources
-    robots, obstacles = load_env('pr2doorway.json')
-
-    # define active DoFs
-    base_joints = [joint_from_name(robots['pr2'], name) for name in PR2_GROUPS['base']]
-
-    collision_fn = get_collision_fn_PR2(robots['pr2'], base_joints, list(obstacles.values()))
-    # Example use of collision checking
-    # print("Robot colliding? ", collision_fn((0.5, -1.3, -np.pi/2)))
-
-    # Example use of setting body poses
-    # set_pose(obstacles['ikeatable6'], ((0, 0, 0), (1, 0, 0, 0)))
-
-    # Example of draw 
-    # draw_sphere_marker((0, 0, 1), 0.1, (1, 0, 0, 1))
     xrange = np.arange(-xlimit, xlimit+lin_res, lin_res)
     yrange = np.arange(-ylimit, ylimit+lin_res, lin_res)
     rrange = np.arange(-np.pi, np.pi, ang_res)
@@ -55,16 +62,7 @@ def main(screenshot=False):
     numy = len(yrange)
     numr = len(rrange)
 
-    # for (x,y) in itertools.product(xrange, yrange):
-    #     draw_sphere_marker((x, y, 1), 0.1, (1, 0, 0, 1))
-    
-    start_config = tuple(get_joint_positions(robots['pr2'], base_joints))
-    goal_config = (2.6, -1.3, -np.pi/2)
-    path = []
 
-    start_time = time.time()
-    ### YOUR CODE HERE ###
-    
     start_config=np.array(start_config)
     goal_config=np.array(goal_config)
     start_config[2] %= 2*np.pi
@@ -202,23 +200,23 @@ def main(screenshot=False):
 
 
             # aggressive optimization; don't key collide cache by rot; but might cause collision
-            # x,y,r = nbrs_coords[i]
-            # k = (x,y)
-            # col=False
-            # if k in collided:
-            #     col=collided[k]
-            # else:
-            #     col=collision_fn((x,y,r))
-            #     collided[k]=col
-
-            # DO key collide cache by rot; but this is slower
-            k = tuple(nbrs_coords[i])
+            x,y,r = nbrs_coords[i]
+            k = (x,y)
+            col=False
             if k in collided:
                 col=collided[k]
             else:
-                col=collision_fn(k)
+                col=collision_fn((x,y,r))
                 collided[k]=col
-            col=collision_fn(k)
+
+            # DO key collide cache by rot; but this is slower
+            # k = tuple(nbrs_coords[i])
+            # if k in collided:
+            #     col=collided[k]
+            # else:
+            #     col=collision_fn(k)
+            #     collided[k]=col
+            # col=collision_fn(k)
 
             if (col):
                 continue
