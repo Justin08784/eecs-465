0068bc12d7904cf82122f691a560d56a3050d8f3
Justin Shin <shine777justin@gmail.com>
2024-10-19 22:36:39 -0400

    cleanup

--- hw3/2d_rr.py
+++ hw3/2d_rr.py
@@ -166,6 +166,17 @@ def main(screenshot=False):
         # q_near = np.where()
 
 
+    # construct path
+    cur = num_nodes - 1
+    path=[]
+    while True:
+        path.append(coords[cur])
+        cur = nbrs_of[cur][0]
+        if cur == 0:
+            break
+    path = path[::-1]
+    # from pprint import pprint
+    # pprint(path[::-1])
 
     # draw tree edges. WARNING: Destructive: destroys nbrs_of
     for lidx in range(num_nodes):
@@ -183,242 +194,10 @@ def main(screenshot=False):
             if not nbrs_of[ridx]:
                 nbrs_of.pop(ridx)
 
-
-
-
-            # for ridx in nbrs_of[lidx]:
-            #     if ridx not in nbrs_of:
-            #         continue
-            #     if not nbrs_of[ridx]:
-            #         nbrs_of.pop(ridx)
-
             draw_line(line_start, line_end, line_width, line_color)
-    wait_for_user()
-    exit(0)
-
-    # These bugs have been addressed:
-    # robot turns AWAY from goal direction momentarily, for some reason
-    # BUG: (66, 14, 3) has a higher f(n) value than (66, 14, 1). Wtf?
-
-    def to_idx(cx, cy, cr):
-        return round((cx + xlimit)/lin_res),\
-               round((cy + ylimit)/lin_res),\
-               round(cr/ang_res)
-    def to_coord(ix, iy, ir):
-        return -xlimit + lin_res*ix,\
-               -ylimit + lin_res*iy,\
-               ang_res*ir
+    # wait_for_user()
 
 
-
-
-    xind, yind, rind = to_idx(start_config[0], start_config[1], start_config[2])
-    goal_closest = np.array(to_idx(goal_config[0], goal_config[1], goal_config[2]))
-
-    def cost(pos1, pos2):
-        return (
-            (pos2[0]-pos1[0])**2+\
-            (pos2[1]-pos1[1])**2+\
-            min(
-                abs(pos2[2]-pos1[2]),
-                2*np.pi - abs(pos2[2]-pos1[2])
-            )**2
-        )**0.5
-
-    def heur(pos):
-        return (
-            (goal_config[0]-pos[0])**2+\
-            (goal_config[1]-pos[1])**2+\
-            min(
-                abs(goal_config[2]-pos[2]),
-                2*np.pi - abs(goal_config[2]-pos[2])
-            )**2
-        )**0.5
-
-    np.set_printoptions(legacy='1.25')
-    frontier = [[heur(start_config), (xind, yind, rind)]]
-    cost_so_far = {(xind, yind, rind) : 0}
-    came_from = {}
-
-    expandee = np.zeros(3, dtype=np.int64)
-    expandee_coords = np.zeros(3)
-    four_connected = False
-    collided = {}
-    if four_connected:
-        num_lin_nbrs, num_ang_nbrs = 4, 2
-        num_nbrs = num_lin_nbrs+num_ang_nbrs
-        nbrs = np.zeros((num_nbrs, 3), dtype=np.int64)
-        nbrs_coords = np.zeros((num_nbrs, 3), dtype=np.float64)
-        costs = np.zeros(num_nbrs, dtype=np.float64)
-        heurs = np.zeros(num_nbrs, dtype=np.float64)
-        in_bound = np.zeros(num_nbrs)
-        pm = [-1,1]
-        incrs = np.array([
-            [1,0,0],
-            [-1,0,0],
-            [0,1,0],
-            [0,-1,0],
-            [0,0,1],
-            [0,0,-1],
-        ])
-
-    else: # eight_connected
-        num_lin_nbrs, num_ang_nbrs = 8, 2
-        num_nbrs = num_lin_nbrs+num_ang_nbrs
-        nbrs = np.zeros((num_nbrs, 3), dtype=np.int64)
-        nbrs_coords = np.zeros((num_nbrs, 3), dtype=np.float64)
-        costs = np.zeros(num_nbrs, dtype=np.float64)
-        heurs = np.zeros(num_nbrs, dtype=np.float64)
-        in_bound = np.zeros(num_nbrs)
-        pm = [-1,1]
-        incrs = np.array([
-            [1,0,0],
-            [-1,0,0],
-            [0,1,0],
-            [0,-1,0],
-            [1,1,0],
-            [1,-1,0],
-            [-1,1,0],
-            [-1,-1,0],
-            [0,0,1],
-            [0,0,-1],
-        ])
-        # assert False, "todo: eight_connected"
- 
-    n=0
-    goal_reached = False
-    while frontier:
-        exf, pos = heapq.heappop(frontier)
-        exg = cost_so_far[pos]
-        expandee[:] = pos
-
-        nbrs[:,:] = expandee[:] + incrs
-        nbrs[:,2] %= numr 
-        in_bound =\
-            (0<=nbrs[:,0])&(nbrs[:,0]<numx)&\
-            (0<=nbrs[:,1])&(nbrs[:,1]<numy)
-            # (0<=nbrs[:,4])&(nbrs[:,4]<numr)
-
-        expandee_coords[:] =\
-            -xlimit + lin_res * expandee[0],\
-            -ylimit + lin_res * expandee[1],\
-            ang_res * expandee[2]
-        nbrs_coords[:,0] = -xlimit + lin_res * nbrs[:,0]
-        nbrs_coords[:,1] = -ylimit + lin_res * nbrs[:,1]
-        nbrs_coords[:,2] = ang_res * nbrs[:,2]
-
-        # g(n)
-        costs[:] = exg + (
-            (expandee_coords[0] - nbrs_coords[:, 0])**2+\
-            (expandee_coords[1] - nbrs_coords[:, 1])**2+\
-            np.minimum(
-                abs(expandee_coords[2] - nbrs_coords[:, 2]),
-                2*np.pi - abs(expandee_coords[2] - nbrs_coords[:, 2])
-            )**2
-        )**0.5
-        # h(n)
-        # print("goal\n", goal_config)
-        # print("expandee", exf, expandee, expandee_coords)
-        heurs[:] = (
-            (goal_config[0] - nbrs_coords[:, 0])**2+\
-            (goal_config[1] - nbrs_coords[:, 1])**2+\
-            np.minimum(
-                abs(goal_config[2] - nbrs_coords[:, 2]),
-                2*np.pi - abs(goal_config[2] - nbrs_coords[:, 2])
-            )**2
-        )**0.5
-        # print("expandee_coords\n", expandee_coords)
-        # print("nbrs\n", nbrs)
-        # print("nbrs_coords\n", nbrs_coords)
-        # print("costs\n", costs)
-        # print("heurs\n", heurs)
-        # print(in_bound)
-
-        cands = np.arange(num_nbrs)
-        for i in cands[in_bound]:
-            nbr_pos = tuple(nbrs[i])
-            nbr_cost = costs[i]
-            nbr_heur = heurs[i]
-
-            # print(nbrs_coords[i], "COLL", collision_fn(nbrs_coords[i]))
-
-
-            # aggressive optimization; don't key collide cache by rot; but might cause collision
-            x,y,r = nbrs_coords[i]
-            k = (x,y)
-            col=False
-            if k in collided:
-                col=collided[k]
-            else:
-                col=collision_fn((x,y,r))
-                collided[k]=col
-
-            # DO key collide cache by rot; but this is slower
-            # k = tuple(nbrs_coords[i])
-            # if k in collided:
-            #     col=collided[k]
-            # else:
-            #     col=collision_fn(k)
-            #     collided[k]=col
-            # col=collision_fn(k)
-
-            if (col):
-                continue
-            if (nbr_pos not in cost_so_far) or (nbr_cost < cost_so_far[nbr_pos]):
-                cost_so_far[nbr_pos] = nbr_cost
-                heapq.heappush(frontier, (nbr_heur + nbr_cost, nbr_pos))
-                came_from[nbr_pos] = pos
-        from pprint import pprint 
-        # pprint(cost_so_far)
-        # pprint(frontier)
-        # pprint(came_from)
-        #if np.all(expandee == (66, 14, 0)):
-        #    # print("ding on")
-        #    # print("nbrs_coords\n", nbrs_coords)
-        #    # print("nbrs\n", nbrs)
-        #    # print("heurs\n", heurs)
-        #    # print("costs\n", costs - exg)
-        #    for x, st in frontier:
-        #        # print(x, st, to_coord(st[0], st[1], st[2]), "prev:", came_from[st])
-        #        pass
-        #    # exit(0)
-
-        if all(expandee == goal_closest):
-            print("Goal reached!")
-            goal_reached = True
-            break  # Stop the A* searchV
-
-    # print(start_config, goal_config)
-    # print(expandee_coords)
-    # exit(0)
-    if not goal_reached:
-        print("No Solution Found.")
-    iidx = to_idx(start_config[0],start_config[1],start_config[2])
-    fidx = to_idx(goal_config[0],goal_config[1],goal_config[2])
-
-
-    path=[]
-    cur=fidx
-    while True:
-        path.append(list(cur))
-        nex = came_from[cur]
-        if nex == iidx:
-            path.append(list(nex))
-            break
-        cur = nex
-    pprint(path[::-1])
-    path=np.array(path, dtype=np.float64)
-    path[:,0] = -xlimit + lin_res * path[:,0]
-    path[:,1] = -ylimit + lin_res * path[:,1]
-    path[:,2] = ang_res * path[:,2]
-    path = path[::-1]
-
-
-    for node in path:
-        draw_sphere_marker((node[0],node[1],1), 0.1, (1, 0, 0, 1))
-
-    print(path)
-    # exit(0)
 
 
 
