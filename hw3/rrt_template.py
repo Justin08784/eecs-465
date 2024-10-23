7f0361f8cedaa17b95a9f6704fb885b5fcc91457
Justin Shin <shine777justin@gmail.com>
2024-10-23 00:54:24 -0400

    more cleanup

--- hw3/rrt_template.py
+++ hw3/rrt_template.py
@@ -8,6 +8,8 @@ import itertools
 from pybullet_tools.utils import wait_for_user
 from utils import draw_line
 import math
+import time
+import random
 
 #########################
 
@@ -38,17 +40,9 @@ def main(screenshot=False):
     # WARNING: remember to disable this (or remove)
     use_precomputed_path = False
     # grid details
-    import time
     start = time.time()
     start_config=np.array(start_config)
     goal_config=np.array(goal_config)
-    # TODO: Why is this not allowed? Is the range restricted to [-np.pi, np.pi]?
-    # start_config[:] %= 2*np.pi
-    # goal_config[:] %= 2*np.pi
-    # BUG: If you do the 2*np.pi normalization, then start_config––and every pt in the main for-loop––
-    # is in collision. e.g. try:
-    # print("ding dong fuck", collision_fn(np.array(start_config)))
-    # exit(0)
 
     jnt_limits = []
     for i in range(6):
@@ -57,93 +51,54 @@ def main(screenshot=False):
         jnt_limits.append([lims[0], lims[1]])
     jnt_limits = np.array(jnt_limits)
 
-    import matplotlib.pyplot as plt
-    def viz_pts(pts):
-        for p in pts:
-            draw_sphere_marker((p[0], p[1], 1), 0.1, (1, 0, 0, 1))
-    def viz_heatmap(pts):
-        xidx = np.rint((pts[:,0] + xlimit)/lin_res).astype(np.int64)
-        yidx = np.rint((pts[:,1] + ylimit)/lin_res).astype(np.int64)
-        grid = np.ones((numx, numy), dtype=int)
-        grid[xidx, yidx] = 0
-
-        plt.imshow(grid, cmap='hot', interpolation='nearest')
-        plt.show()
-        return
-
     def fill_random(q_rand):
         for i in range(6):
             q_rand[:,i] = np.random.uniform(low=jnt_limits[i,0], high=jnt_limits[i,1], size=q_rand.shape[0])
 
     num_nodes = 0
     tree_len = 16
-    coords = np.zeros((tree_len, 6), dtype=np.float64) # you should exponentially resize this array
+    coords = np.zeros((tree_len, 6), dtype=np.float64) # exponentially resized
     coords[:] = np.inf
-    nbrs_of = {} # maps each index to its nbrs
+    nbrs_of = {} # maps each index to its nbrs (first nbr is always parent)
     step_size = 0.05
     goal_bias = 0.1
-    # visualize RRT using python heatmap
 
-    cur_rand = 0
     rand_len = 1024
     q_rand = np.zeros((rand_len, 6), dtype=np.float64)
     fill_random(q_rand)
 
+    # add root (start)
     coords[0] = start_config
     nbrs_of[0] = []
     num_nodes = 1
-
-
-    # puts the point at height=1.5 so it's better visible
     goal_reached = False
-
     rand_idx = 0
-    iter = 0
     while (not goal_reached and not use_precomputed_path):
         if rand_idx >= rand_len:
             # refill rand array
             fill_random(q_rand)
             rand_idx = 0
 
-        import random
         cur_rand = q_rand[rand_idx] if random.random() >= goal_bias else goal_config
         cur_rand[2] = 0
-        # cur_rand = np.array([1,0,0])
 
         dists_sq = np.minimum(
             np.sum((coords[:num_nodes] - cur_rand)**2, axis=1),
             np.sum((2*np.pi - abs(coords[:num_nodes] - cur_rand))**2, axis=1)
         )**0.5
-        # np.minimum(
-        #     abs(expandee_coords[2] - nbrs_coords[:, 2]),
-        #     2*np.pi - abs(expandee_coords[2] - nbrs_coords[:, 2])
-        # )**2
+
         min_idx = int(np.argmin(dists_sq))
         cur_near = coords[min_idx]
         uvec = (cur_rand - cur_near)/dists_sq[min_idx]
-        # if iter % 1000 == 0:
-        #     print(iter, dists_sq[min_idx])
-        # iter+=1
 
-        # print(cur_rand, cur_near, uvec)
-        # print("dists", dists_sq[0])
-        # draw_sphere_marker(get_high(cur_near), 0.1, (0, 1, 0, 1))
-        # draw_sphere_marker(get_high(cur_rand), 0.1, (0, 1, 0, 1))
         max_step = int(dists_sq[min_idx]/step_size)
         prev_idx = min_idx
         cur_idx = num_nodes
-        # print("vecs")
-        # print(cur_near)
-        # print(cur_rand)
-        # print(uvec)
-        # print("Default")
-        # print(collision_fn(start_config))
+
         for t in range(1, max_step + 1):
             pt = cur_near+t*step_size*uvec
             if (collision_fn(pt)):
                 break
-            # print(collision_fn(pt))
-            # assert(False)
             goal_reached = np.sum((pt - goal_config)**2)**(0.5) < step_size
 
             if (num_nodes >= tree_len):
@@ -156,28 +111,13 @@ def main(screenshot=False):
             num_nodes += 1
 
             coords[cur_idx] = pt
-            # init[cur_idx] = True
             nbrs_of[prev_idx].append(cur_idx)
             nbrs_of[cur_idx] = [prev_idx]
 
             prev_idx = cur_idx
             cur_idx += 1
-            # draw_sphere_marker(get_high(pt), 0.1, (1, 0, 0, 1))
 
         rand_idx += 1
-        # if (rand_idx == 100):
-        #     wait_for_user()
-        # from pprint import pprint
-        # print("Done. Press enter to continue")
-        # print("coords\n", len(coords))
-        # pprint(coords)
-        # print("init\n", len(init))
-        # pprint(init)
-        # print("nbrs_of\n", len(nbrs_of))
-        # pprint(nbrs_of)
-        # wait_for_user()
-        # exit(0)
-        # q_near = np.where()
 
 
     # construct path
@@ -196,18 +136,9 @@ def main(screenshot=False):
         path=np.load("raw_path.npy")
     print("runtime: ", time.time() - start)
 
-    #ee_pose[0] is the translation of the left gripper tool frame
-    #ee_pose[1] is the rotation (represented as a quaternion the left gripper tool frame), we don't need this
-    def get_ee_positions(path):
-        PR2 = robots['pr2']
-        path_positions = []
-        for st in path:
-            set_joint_positions(PR2, joint_idx, st)
-            path_positions.append(get_link_pose(PR2, link_from_name(PR2, 'l_gripper_tool_frame'))[0])
-        return path_positions
-
     num_iters = 150
     params = np.sort(np.random.uniform(low=0,high=1.0,size=(num_iters,2)), axis=1)
+
 
     q_dim = 6
     arr_len = path.shape[0]
@@ -276,6 +207,7 @@ def main(screenshot=False):
             continue
 
         nex[:lidx] = cur[:lidx]
+        nex[lidx+2:nex_num_points] = cur[ridx:num_points]
 
         nex[lidx,:q_dim] = lq
         nex[lidx,q_dim] = llen - cumsums[lidx-1]
@@ -283,7 +215,6 @@ def main(screenshot=False):
         nex[lidx+1,:q_dim] = rq
         nex[lidx+1,q_dim] = vec_norm
 
-        nex[lidx+2:nex_num_points] = cur[ridx:num_points]
         nex[lidx+2, q_dim] = np.sum((nex[lidx+2,:q_dim] - nex[lidx+1,:q_dim])**2)**(1/2)
 
         num_points = nex_num_points
@@ -293,8 +224,16 @@ def main(screenshot=False):
         continue
 
     smoothed_path = cur[:num_points,:q_dim]
-    
-    # np.save("raw_path.npy", path)
+
+
+    def get_ee_positions(path):
+        PR2 = robots['pr2']
+        path_positions = []
+        for st in path:
+            set_joint_positions(PR2, joint_idx, st)
+            path_positions.append(get_link_pose(PR2, link_from_name(PR2, 'l_gripper_tool_frame'))[0])
+        return path_positions
+
     raw_positions = get_ee_positions(path)
     for i in range(len(raw_positions) - 1):
         line_start = raw_positions[i]
@@ -312,30 +251,6 @@ def main(screenshot=False):
         draw_line(line_start, line_end, line_width, line_color)
     wait_for_user()
     path=smoothed_path
-
-    # from pprint import pprint
-    # pprint(path[::-1])
-
-    # draw tree edges. WARNING: Destructive: destroys nbrs_of
-    # for lidx in range(num_nodes):
-    #     if lidx not in nbrs_of:
-    #         continue
-    #     for ridx in nbrs_of[lidx]:
-    #         line_start = get_high(coords[lidx])
-    #         line_end = get_high(coords[ridx])
-    #         line_width = 1
-    #         line_color = (1, 0, 0) # R, G, B
-
-    #         if ridx not in nbrs_of:
-    #             continue
-    #         nbrs_of[ridx].remove(lidx)
-    #         if not nbrs_of[ridx]:
-    #             nbrs_of.pop(ridx)
-
-    #         draw_line(line_start, line_end, line_width, line_color)
-    # wait_for_user()
-
-
 
     new_path = [cur[0,:q_dim]]
     step_size = 0.01
