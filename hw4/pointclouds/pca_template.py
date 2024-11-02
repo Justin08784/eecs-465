63479f0d3ed7cc56cef861cb48863a33ec5f89ac
Justin Shin <shine777justin@gmail.com>
2024-11-02 06:11:15 -0400

    3.a,b. But unsure: why U instead of V^T?

--- hw4/pointclouds/pca_template.py
+++ hw4/pointclouds/pca_template.py
@@ -4,6 +4,8 @@ import numpy
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 ###YOUR IMPORTS HERE###
+import scipy as sp
+import numpy as np
 
 ###YOUR IMPORTS HERE###
 
@@ -14,10 +16,33 @@ def main():
     pc = utils.load_pc('cloud_pca.csv')
 
     ###YOUR CODE HERE###
+    pc = np.array(pc)
+    X = pc[:,:,0]
+    m = pc.shape[1] # point dimension
+    n = pc.shape[0] # number of points
     # Show the input point cloud
     fig = utils.view_pc([pc])
 
     #Rotate the points to align with the XY plane
+    mean = np.mean(X, axis=0)
+    X -= mean # center to mean
+
+    Y = X.T / ((n-1)**0.5)
+    U, S, V_T = sp.linalg.svd(Y)
+
+    var = S**2
+    threshold = 1e-2
+    keep = var >= threshold
+
+    # BUG: The slide say to use V_T, but if I do that
+    # then the points vanish (i.e. they all go to (0,0,0)).
+    # How is that using U instead of V_T works?
+    # NOTE: 3.a) rotation only
+    # pc[:,:,0] = X @ U
+    # NOTE: 3.b) rotation + dimensionality reduction
+    pc[:,keep,0] = X @ U[:,keep]
+    pc[:,~keep,0] = 0
+    fig = utils.view_pc([pc])
 
 
 
