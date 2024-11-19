300facc34f3463790a5994879137b94ad4dd521b
Justin Shin <shine777justin@gmail.com>
2024-11-18 23:59:52 -0500

    should be np.array

--- hw5/kfmodel.py
+++ hw5/kfmodel.py
@@ -3,13 +3,13 @@ import numpy as np
 ###YOUR CODE HERE###
 #motion model
 A = np.eye(2) #replace with your A
-B = np.eye([
+B = np.array([
     [1.5, 0.1],
     [0.2, -0.5]
 ]) #replace with your B
 
 #sensor model
-C = np.eye([
+C = np.array([
     [1.05, 0.01],
     [0.01, 0.90]
 ]) #replace with your C
