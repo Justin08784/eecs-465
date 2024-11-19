0fdab5b48cc8e3f0a02a03c47d871b57ac4f8c2a
Justin Shin <shine777justin@gmail.com>
2024-11-19 00:48:18 -0500

    impl kf.py

--- hw5/kf.py
+++ hw5/kf.py
@@ -23,13 +23,14 @@ def KalmanFilter(mu, Sigma, z, u, A, B, C, Q, R):
     ###YOUR CODE HERE###
 
     #prediction step    
+    mu_bar = A @ mu + B @ u
+    Sigma_bar = A @ Sigma @ A.T + R
     
 
     #correction step
-
-
-
-    mu_new = mu; Sigma_new = Sigma #comment this out to use your code
+    K = Sigma_bar @ C @ np.linalg.inv(C @ Sigma_bar @ C.T + Q)
+    mu_new = mu_bar + K @ (z - C @ mu_bar)
+    Sigma_new = (np.eye(2) - K @ C) @ Sigma_bar
     ###YOUR CODE HERE###
     return mu_new, Sigma_new
 
