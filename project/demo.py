47d7f3ec6f20f4ae22a7c92e462977d35ed963d2
Justin Shin <shine777justin@gmail.com>
2024-11-30 17:27:33 -0500

    finish init control set

--- project/demo.py
+++ project/demo.py
@@ -10,6 +10,7 @@ import pybullet as p
 import time
 from pprint import pprint
 from simulator import simulate
+import itertools
 
 '''
 Initialization functions
@@ -75,46 +76,50 @@ u0 = np.array([0, 0, 0, 0], dtype=np.float64) # a_x, a_y, a_z, \alpha
 # goal configuration
 sg = np.array([2, 2, ROBOT_Z, 0], dtype=np.float64) # x, y, theta
 # vg = np.array([0, 0, 0], dtype=np.float64) # v_x, v_y, \omega
-CONTROL_ORI_RES = (45) * np.pi/180 # degrees (specify in parens)
-CONTROL_MAG_RES = 0.2              # ms^-1
+CONTROL_LIN_ORI_RES = (45) * np.pi/180 # degrees (specify in parens)
+CONTROL_LIN_MAG_RES = 0.5              # ms^-1
 CONTROL_ANG_RES = 0.2
 CONTROL_SET = None
 
 def init_control_set():
     # Part 1: initialize linear controls
-    NUM_ABS_ORIS = int(2*np.pi / CONTROL_ORI_RES)
-    NUM_ABS_MAGS = int(MAX_LIN_ACCEL / CONTROL_MAG_RES)
+    num_lin_oris = int(2*np.pi / CONTROL_LIN_ORI_RES)
+    num_lin_mags = int(MAX_LIN_ACCEL / CONTROL_LIN_MAG_RES)
 
     # compute non-zero linear control magnitudes
-    MAGS_BASE = np.arange(1, NUM_ABS_MAGS + 1)
-    LIN_MAGS = np.zeros(2 * NUM_ABS_MAGS, np.float64)
-    LIN_MAGS[:NUM_ABS_MAGS] = CONTROL_MAG_RES * MAGS_BASE
-    LIN_MAGS[NUM_ABS_MAGS:] = CONTROL_MAG_RES * MAGS_BASE * (-1)
+    mags_base = np.arange(1, num_lin_mags + 1)
+    lin_mags = np.concatenate((mags_base, -mags_base)) * CONTROL_LIN_MAG_RES
 
     # compute linear controls with non-zero magnitudes
-    LIN_ORIS = CONTROL_ORI_RES * np.arange(NUM_ABS_ORIS)
-    LINX = np.outer(LIN_MAGS, np.cos(LIN_ORIS))
-    LINY = np.outer(LIN_MAGS, np.sin(LIN_ORIS))
+    lin_oris = CONTROL_LIN_ORI_RES * np.arange(num_lin_oris)
+    xs = np.outer(lin_mags, np.cos(lin_oris)).flatten()
+    ys = np.outer(lin_mags, np.sin(lin_oris)).flatten()
 
     # add the null linear control (i.e. linear control with zero magnitude)
-    LINX = np.insert(LINX, 0, 0)
-    LINY = np.insert(LINY, 0, 0)
-    print(LINX, LINX.shape)
+    xs = np.insert(xs, 0, 0)
+    ys = np.insert(ys, 0, 0)
+    lin_vs = np.column_stack([xs, ys])
 
     # Part 2: initialize angular controls
-    num_abs_angv = int(MAX_ANG_ACCEL / CONTROL_ANG_RES)
-    angv = CONTROL_ANG_RES * np.arange(-num_abs_angv, num_abs_angv + 1)
+    num_angv = int(MAX_ANG_ACCEL / CONTROL_ANG_RES)
+    ang_vs = CONTROL_ANG_RES * np.arange(-num_angv, num_angv + 1)
+    # print(ang_vs, ang_vs.shape)
+    # print(lin_mags, lin_mags.shape)
+
+    num_lins = lin_vs.shape[0]
+    num_angs = ang_vs.shape[0]
+    CONTROL_SET = np.zeros((num_angs, num_lins, 3))
+    CONTROL_SET[:, :, :2] = lin_vs
+    CONTROL_SET[:, :, 2] = ang_vs[:, np.newaxis]
 
     import matplotlib.pyplot as plt
-    plt.scatter(LINX, LINY)
+    plt.scatter(CONTROL_SET[0,:,0], CONTROL_SET[0,:,1])
     plt.show()
 
 init_control_set()
 
 
 
-print(LIN_MAGS)
-print(LIN_ORIS)
 exit(0)
 
 CONTROL_ANG_RES = 0.2
