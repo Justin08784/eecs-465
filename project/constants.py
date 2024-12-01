384bdeda1b5ba293c39d9d16155abc50cb04020c
Justin Shin <shine777justin@gmail.com>
2024-12-01 15:09:20 -0500

    SIG REORG: constants moved to its own file

---
+++ project/constants.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+'''
+Physical constants
+'''
+WALL_HEIGHT = 0.4
+MAX_LIN_ACCEL = 1
+MAX_ANG_ACCEL = 1
+MAX_LIN_VEL = 2 # does nothing currently
+MAX_ANG_VEL = 2 # does nothing currently
+# WARNING: hardcoded room dimensions; confirm in env_json of load_env
+XLIMIT = 2.6
+YLIMIT = 2.6
+
+# Simulation
+dt = 0.01       # the resolution to which we are simulating
+dt_sim = 0.1    # minimum time interval to apply a control
+NUM_SIM_STEPS = int(dt_sim/dt)
+epsilon = 0.01  # if metric(s1, s2) < epsilon, then the states are "equivalent"
+
+'''
+Robot state
+'''
+# initial configuration
+ROBOT_Z = WALL_HEIGHT/2
+s0 = np.array([2, -2, ROBOT_Z, 0], dtype=np.float64) # x, y, z, theta
+v0 = np.array([0, 0, 0, 0], dtype=np.float64) # v_x, v_y, v_z, \omega
+u0 = np.array([0, 0, 0, 0], dtype=np.float64) # a_x, a_y, a_z, \alpha
+
+
+# goal configuration
+sg = np.array([2, 2, ROBOT_Z, 0], dtype=np.float64) # x, y, theta
+# vg = np.array([0, 0, 0], dtype=np.float64) # v_x, v_y, \omega
+CONTROL_LIN_ORI_RES = (45) * np.pi/180 # degrees (specify in parens)
+CONTROL_LIN_MAG_RES = 0.5              # ms^-1
+CONTROL_ANG_RES = 1
+CONTROL_SET = None
+NUM_CONTROL_PRIMITIVES = None
+
+def init_control_set():
+    global CONTROL_SET
+    global NUM_CONTROL_PRIMITIVES
+    # Part 1: initialize linear controls
+    num_lin_oris = int(2*np.pi / CONTROL_LIN_ORI_RES)
+    num_lin_mags = int(MAX_LIN_ACCEL / CONTROL_LIN_MAG_RES)
+
+    # compute non-zero linear control magnitudes
+    mags_base = np.arange(1, num_lin_mags + 1)
+    lin_mags = np.concatenate((mags_base, -mags_base)) * CONTROL_LIN_MAG_RES
+
+    # compute linear controls with non-zero magnitudes
+    lin_oris = CONTROL_LIN_ORI_RES * np.arange(num_lin_oris)
+    xs = np.outer(lin_mags, np.cos(lin_oris)).flatten()
+    ys = np.outer(lin_mags, np.sin(lin_oris)).flatten()
+
+    # add the null linear control (i.e. linear control with zero magnitude)
+    xs = np.insert(xs, 0, 0)
+    ys = np.insert(ys, 0, 0)
+    lin_vs = np.column_stack([xs, ys])
+
+    # Part 2: initialize angular controls
+    num_angv = int(MAX_ANG_ACCEL / CONTROL_ANG_RES)
+    ang_vs = CONTROL_ANG_RES * np.arange(-num_angv, num_angv + 1)
+    # print(ang_vs, ang_vs.shape)
+    # print(lin_mags, lin_mags.shape)
+
+    num_lins = lin_vs.shape[0]
+    num_angs = ang_vs.shape[0]
+    CONTROL_SET = np.zeros((num_angs, num_lins, 3))
+    CONTROL_SET[:, :, :2] = lin_vs
+    CONTROL_SET[:, :, 2] = ang_vs[:, np.newaxis]
+    CONTROL_SET = CONTROL_SET.reshape(-1, CONTROL_SET.shape[2])
+    NUM_CONTROL_PRIMITIVES = CONTROL_SET.shape[0]
+    # print(CONTROL_SET.shape)
+    # print(CONTROL_SET)
+
+    # import matplotlib.pyplot as plt
+    # plt.scatter(CONTROL_SET[:num_lins,0], CONTROL_SET[:num_lins,1])
+    # plt.show()
+
+'''
+Tree
+'''
+GOAL_BIAS = 0.1
+STEP_SIZE = 0.1
+
+IDX_POS = np.arange(4)
+IDX_VEL = np.arange(4, 8)
+
+
+'''
+Random tape
+'''
+RAND_LEN = 1000
--- project/demo.py
+++ project/demo.py
@@ -12,6 +12,7 @@ from pprint import pprint
 from simulator import simulate
 import itertools
 import math
+import constants as c
 
 '''
 Initialization functions
@@ -23,130 +24,44 @@ def create_drone(x, y, theta):
     visual_shape = p.createVisualShape(p.GEOM_BOX, halfExtents=half_extents, rgbaColor=[0, 1, 0, 1])
     body_id = p.createMultiBody(baseCollisionShapeIndex=collision_shape,
                                 baseVisualShapeIndex=visual_shape,
-                                basePosition=(x,y,WALL_HEIGHT/2),
+                                basePosition=(x,y,c.ROBOT_Z),
                                 baseOrientation=p.getQuaternionFromEuler((0,0,theta)))
     return body_id
 
 
 def create_wall(x, y, theta, len):
-    half_extents = [0.1, len / 2, WALL_HEIGHT / 2]
+    half_extents = [0.1, len / 2, c.WALL_HEIGHT / 2]
     collision_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=half_extents)
     visual_shape = p.createVisualShape(p.GEOM_BOX, halfExtents=half_extents, rgbaColor=[1, 0, 0, 0.5])
     body_id = p.createMultiBody(baseCollisionShapeIndex=collision_shape,
                                 baseVisualShapeIndex=visual_shape,
-                                basePosition=(x,y,WALL_HEIGHT/2),
+                                basePosition=(x,y,c.WALL_HEIGHT/2),
                                 baseOrientation=p.getQuaternionFromEuler((0,0,theta)))
     return body_id
 
 
 def create_cylinder(x, y, r):
-    collision_shape = p.createCollisionShape(p.GEOM_CYLINDER, radius=r, height=WALL_HEIGHT)
-    visual_shape = p.createVisualShape(p.GEOM_CYLINDER, radius=r, length=WALL_HEIGHT, rgbaColor=[1, 0, 0, 0.5])
+    collision_shape = p.createCollisionShape(p.GEOM_CYLINDER, radius=r, height=c.WALL_HEIGHT)
+    visual_shape = p.createVisualShape(p.GEOM_CYLINDER, radius=r, length=c.WALL_HEIGHT, rgbaColor=[1, 0, 0, 0.5])
     body_id = p.createMultiBody(baseCollisionShapeIndex=collision_shape,
                                 baseVisualShapeIndex=visual_shape,
-                                basePosition=(x,y,WALL_HEIGHT/2),
+                                basePosition=(x,y,c.WALL_HEIGHT/2),
                                 baseOrientation=p.getQuaternionFromEuler((0,0,0)))
     return body_id
 
 
-'''
-Physical constants
-'''
-WALL_HEIGHT = 0.4
-MAX_LIN_ACCEL = 1
-MAX_ANG_ACCEL = 1
-MAX_LIN_VEL = 2
-MAX_ANG_VEL = 2
-
-# WARNING: hardcoded room dimensions; confirm in env_json of load_env
-XLIMIT = 2.6
-YLIMIT = 2.6
-dt = 0.01       # the resolution to which we are simulating
-dt_sim = 0.1    # minimum time interval to apply a control
-epsilon = 0.01  # if metric(s1, s2) < epsilon, then the states are "equivalent"
-
-'''
-Robot state
-'''
-# initial configuration
-ROBOT_Z = WALL_HEIGHT/2
-s0 = np.array([2, -2, ROBOT_Z, 0], dtype=np.float64) # x, y, z, theta
-v0 = np.array([0, 0, 0, 0], dtype=np.float64) # v_x, v_y, v_z, \omega
-u0 = np.array([0, 0, 0, 0], dtype=np.float64) # a_x, a_y, a_z, \alpha
-
-
-# goal configuration
-sg = np.array([2, 2, ROBOT_Z, 0], dtype=np.float64) # x, y, theta
-# vg = np.array([0, 0, 0], dtype=np.float64) # v_x, v_y, \omega
-CONTROL_LIN_ORI_RES = (45) * np.pi/180 # degrees (specify in parens)
-CONTROL_LIN_MAG_RES = 0.5              # ms^-1
-CONTROL_ANG_RES = 1
-CONTROL_SET = None
-
-def init_control_set():
-    global CONTROL_SET
-    # Part 1: initialize linear controls
-    num_lin_oris = int(2*np.pi / CONTROL_LIN_ORI_RES)
-    num_lin_mags = int(MAX_LIN_ACCEL / CONTROL_LIN_MAG_RES)
-
-    # compute non-zero linear control magnitudes
-    mags_base = np.arange(1, num_lin_mags + 1)
-    lin_mags = np.concatenate((mags_base, -mags_base)) * CONTROL_LIN_MAG_RES
-
-    # compute linear controls with non-zero magnitudes
-    lin_oris = CONTROL_LIN_ORI_RES * np.arange(num_lin_oris)
-    xs = np.outer(lin_mags, np.cos(lin_oris)).flatten()
-    ys = np.outer(lin_mags, np.sin(lin_oris)).flatten()
-
-    # add the null linear control (i.e. linear control with zero magnitude)
-    xs = np.insert(xs, 0, 0)
-    ys = np.insert(ys, 0, 0)
-    lin_vs = np.column_stack([xs, ys])
-
-    # Part 2: initialize angular controls
-    num_angv = int(MAX_ANG_ACCEL / CONTROL_ANG_RES)
-    ang_vs = CONTROL_ANG_RES * np.arange(-num_angv, num_angv + 1)
-    # print(ang_vs, ang_vs.shape)
-    # print(lin_mags, lin_mags.shape)
-
-    num_lins = lin_vs.shape[0]
-    num_angs = ang_vs.shape[0]
-    CONTROL_SET = np.zeros((num_angs, num_lins, 3))
-    CONTROL_SET[:, :, :2] = lin_vs
-    CONTROL_SET[:, :, 2] = ang_vs[:, np.newaxis]
-    CONTROL_SET = CONTROL_SET.reshape(-1, CONTROL_SET.shape[2])
-    # print(CONTROL_SET.shape)
-    # print(CONTROL_SET)
-
-    # import matplotlib.pyplot as plt
-    # plt.scatter(CONTROL_SET[:num_lins,0], CONTROL_SET[:num_lins,1])
-    # plt.show()
-
-init_control_set()
 
 '''
 Simulated states
 '''
-NUM_SIM_STEPS = int(dt_sim/dt)
-# NUM_SIM_STEPS = 300
-sim_states = np.zeros((CONTROL_SET.shape[0], NUM_SIM_STEPS, 8), dtype=np.float64)
-
+sim_states = None
 
 '''
 Tree
 '''
-GOAL_BIAS = 0.1
-STEP_SIZE = 0.1
 tree_len = 128 # num allocated entries (i.e. size of tree array)
 tree_cur = 0  # num used entries; equiv, next free idx
-
-state_tree = np.zeros((tree_len, 8), dtype=np.float64)
-IDX_POS = np.arange(4)
-IDX_VEL = np.arange(4, 8)
-state_tree[:] = np.inf
-state_tree[0,IDX_POS] = s0
-state_tree[0,IDX_VEL] = v0
-tree_cur = 1
+state_tree = None
 
 nbrs_of = {} # maps each index to its nbrs
 nbrs_of[0] = []
@@ -156,26 +71,38 @@ goal_reached = False
 '''
 Random tape
 '''
-RAND_LEN = 1000
 rand_cur = 0
-state_rand = np.zeros((RAND_LEN, 8), dtype=np.float64)
+state_rand = None
 
 def fill_random(rand):
     # initialize poses
-    rand[:,0] = np.random.uniform(low=-XLIMIT, high=XLIMIT, size=RAND_LEN)
-    rand[:,1] = np.random.uniform(low=-YLIMIT, high=YLIMIT, size=RAND_LEN)
-    rand[:,2] = ROBOT_Z
-    rand[:,3] = np.random.uniform(low=0, high=2*np.pi, size=RAND_LEN)
+    rand[:,0] = np.random.uniform(low=-c.XLIMIT, high=c.XLIMIT, size=c.RAND_LEN)
+    rand[:,1] = np.random.uniform(low=-c.YLIMIT, high=c.YLIMIT, size=c.RAND_LEN)
+    rand[:,2] = c.ROBOT_Z
+    rand[:,3] = np.random.uniform(low=0, high=2*np.pi, size=c.RAND_LEN)
 
     # initialize velocities
-    v_mags = np.random.uniform(low=0, high=MAX_LIN_VEL, size=RAND_LEN)
+    v_mags = np.random.uniform(low=0, high=c.MAX_LIN_VEL, size=c.RAND_LEN)
 
     rand[:,4] = v_mags * np.cos(rand[:,3])
     rand[:,5] = v_mags * np.sin(rand[:,3])
     rand[:,6] = 0
-    rand[:,7] = np.random.uniform(low=-MAX_ANG_VEL, high=MAX_ANG_VEL, size=RAND_LEN)
-fill_random(state_rand)
+    rand[:,7] = np.random.uniform(low=-c.MAX_ANG_VEL, high=c.MAX_ANG_VEL, size=c.RAND_LEN)
 
+def init_globals():
+    global sim_states
+    sim_states = np.zeros((c.NUM_CONTROL_PRIMITIVES, c.NUM_SIM_STEPS, 8), dtype=np.float64)
+
+    global state_tree
+    state_tree = np.zeros((tree_len, 8), dtype=np.float64)
+    state_tree[:] = np.inf
+    state_tree[0,c.IDX_POS] = c.s0
+    state_tree[0,c.IDX_VEL] = c.v0
+    tree_cur = 1
+    
+    global state_rand
+    state_rand = np.zeros((c.RAND_LEN, 8), dtype=np.float64)
+    fill_random(state_rand)
 
 # BUG:
 # Sometimes the path goes OOB beyond the boundary walls. I think this happens
@@ -183,21 +110,17 @@ fill_random(state_rand)
 # the wall can easily fit between two adjacent collision checks.
 
 def main(screenshot=False):
-    global dt
-    global dt_sim
-    global s0
-    global v0
-    global u0
-
     global goal_reached
     global rand_cur
     global state_tree, tree_len, tree_cur
+    c.init_control_set()
+    init_globals()
 
     # initialize PyBullet
     connect(use_gui=True)
     # load robot and floor/walls 
     _, obstacles = load_env('envs/2D_drone.json')
-    robot_id = create_drone(s0[0], s0[1], s0[3])
+    robot_id = create_drone(c.s0[0], c.s0[1], c.s0[3])
     obstacle_ids = list(obstacles.values())
     assert(not get_joints(robot_id))
 
@@ -214,7 +137,7 @@ def main(screenshot=False):
     '''
     Helpers
     '''
-    def execute_trajectory(states, dt):
+    def execute_trajectory(states, dt=c.dt):
         for i in range(len(states)):
             x, y, z, theta = states[i,:4]
             set_pose(robot_id, ((x, y, z), p.getQuaternionFromEuler((0,0,theta))))
@@ -222,28 +145,28 @@ def main(screenshot=False):
 
 
     # num_states = 1000
-    tmp_sg = np.array([1, -1.2, ROBOT_Z, np.pi/2], dtype=np.float64) # x, y, theta
+    tmp_sg = np.array([1, -1.2, c.ROBOT_Z, np.pi/2], dtype=np.float64) # x, y, theta
     draw_sphere_marker(tmp_sg[:3], 0.1, (0, 1, 0, 1))
-    # free = ~np.zeros(CONTROL_SET.shape[0])
-    curs = s0.copy()
-    curv = v0.copy()
+    # free = ~np.zeros(c.NUM_CONTROL_PRIMITIVES)
+    curs = c.s0.copy()
+    curv = c.v0.copy()
     found = False
-    start = time.time()
     MAX_NUM_EXTENDS = 200
-    errors = np.zeros(CONTROL_SET.shape[0], dtype=np.float64)
+    errors = np.zeros(c.NUM_CONTROL_PRIMITIVES, dtype=np.float64)
     errors[:] = np.inf
     prev_min_error = np.inf
     curr_min_error = np.inf
-    argmin = np.zeros(CONTROL_SET.shape[0], dtype=int)
+    argmin = np.zeros(c.NUM_CONTROL_PRIMITIVES, dtype=int)
 
+    start = time.time()
     for i in range(MAX_NUM_EXTENDS):
-        simulate(CONTROL_SET, curs, curv, sim_states, NUM_SIM_STEPS, dt)
-        for c in range(CONTROL_SET.shape[0]):
-            col_t = NUM_SIM_STEPS # first colliding time step
-            for t in range(NUM_SIM_STEPS):
+        simulate(c.CONTROL_SET, curs, curv, sim_states, c.NUM_SIM_STEPS)
+        for ctrl in range(c.NUM_CONTROL_PRIMITIVES):
+            col_t = c.NUM_SIM_STEPS # first colliding time step
+            for t in range(c.NUM_SIM_STEPS):
                 # look for first collision (if any)
-                pos = sim_states[c,t,:3]
-                quat = p.getQuaternionFromEuler((0,0,sim_states[c,t,3]))
+                pos = sim_states[ctrl,t,:3]
+                quat = p.getQuaternionFromEuler((0,0,sim_states[ctrl,t,3]))
                 if collision_fn((pos, quat)):
                     # collided
                     col_t = t
@@ -260,15 +183,15 @@ def main(screenshot=False):
             dists_sq = (
                 # NOTE: if you change lin_error power from 2 to 12, it corrects
                 # more aggressively at longer distances, leading to better perf
-                lin_w*np.sum((sim_states[c,:col_t,:3] - tmp_sg[:3])**2, axis=1)+\
+                lin_w*np.sum((sim_states[ctrl,:col_t,:3] - tmp_sg[:3])**2, axis=1)+\
                 ang_w*np.minimum(
-                    abs(sim_states[c,:col_t,3] - tmp_sg[3]),
-                    2*np.pi - abs(sim_states[c,:col_t,3] - tmp_sg[3])
+                    abs(sim_states[ctrl,:col_t,3] - tmp_sg[3]),
+                    2*np.pi - abs(sim_states[ctrl,:col_t,3] - tmp_sg[3])
                 )**2
             )**0.5
-            argmin[c] = np.argmin(dists_sq)
-            errors[c] = dists_sq[argmin[c]]
-            if errors[c] < epsilon:
+            argmin[ctrl] = np.argmin(dists_sq)
+            errors[ctrl] = dists_sq[argmin[ctrl]]
+            if errors[ctrl] < c.epsilon:
                 print("Found!")
                 found = True
                 break
@@ -307,9 +230,8 @@ def main(screenshot=False):
             break
     print(time.time() - start)
 
-
     # execute_trajectory(np.array(slst), dt_sim)
-    execute_trajectory(state_tree[:tree_cur,:4], dt)
+    execute_trajectory(state_tree[:tree_cur,:4])
     exit(0)
 
     # print(">>>>")
