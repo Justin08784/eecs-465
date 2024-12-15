1963eede10dd78bf3b12dd60281e00cc6eccc6c8
Justin Shin <shine777justin@gmail.com>
2024-12-15 12:56:11 -0500

    reorg analysis

--- project/analysis.py
+++ project/analysis.py
@@ -1,28 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+LIN_ORIS = 0
+LIN_MAGS = 1
+ANG_MAGS = 2
+choose = LIN_ORIS
 
-df = pd.read_csv("oris.csv")
-print(df)
-df1 = df.groupby("ori_res").agg(['mean'])
-df2 = df.groupby("ori_res").agg(['mean', 'std'])
-print(df2.columns)
-print(df2[("seed", "mean")])
+if choose == LIN_ORIS:
+    df = pd.read_csv("oris.csv")
+    df1 = df.groupby("ori_res").agg(['mean'])
+    df2 = df.groupby("ori_res").agg(['mean', 'std'])
+    plt.errorbar(
+        df2.index,
+        df2[("runtime", "mean")],
+        yerr=df2[("runtime", "std")],
+        fmt='o',
+        capsize=5,
+    )
+    plt.ylabel("Runtime (seconds)")
+    plt.xlabel("Linear acceleration orientation resolution (degrees)")
+    plt.show()
+    exit(0)
 
-# plt.errorbar(
-#     df2.index,
-#     df2[("runtime", "mean")],
-#     yerr=df2[("runtime", "std")],
-#     fmt='o',
-#     capsize=5,
-# )
-plt.errorbar(
-    df2.index,
-    df2[("total_len", "mean")],
-    yerr=df2[("total_len", "std")],
-    fmt='o',
-    capsize=5,
-)
-plt.ylabel("Runtime (seconds)")
-plt.xlabel("Linear acceleration orientation resolution (degrees)")
-plt.show()
+if choose == LIN_MAGS:
+    print("dipshit")
+    exit(0)
+
+if choose == ANG_MAGS:
+    exit(0)
+
