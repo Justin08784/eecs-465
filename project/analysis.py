7904e5958a7068e4941fcb2b7d3bce0721941bcc
Justin Shin <shine777justin@gmail.com>
2024-12-16 00:59:38 -0500

    data col

--- project/analysis.py
+++ project/analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 LIN_ORIS = 0
 LIN_MAGS = 1
@@ -7,6 +8,9 @@ ANG_MAGS = 2
 choose = LIN_ORIS
 
 if choose == LIN_ORIS:
+
+    num_primitives = np.log(np.array([873, 441, 85, 65, 15, 9]))
+
     # Interesting:
     # - runtime / num_targets
     # - work / total_len
@@ -15,19 +19,22 @@ if choose == LIN_ORIS:
     # - ** runtime / num_nodes
     # - ** total_delta_ang / num_nodes
     df = pd.read_csv("combined2.csv")
-    df["total_delta_ang"] /= df["num_nodes"]
-    df1 = df.groupby("ori_res").agg(['mean'])
     df2 = df.groupby("ori_res").agg(['mean', 'std'])
+    df2[("work", "mean")] /= df2[("total_len", "mean")]
+
+    df2["num_primitives"] = num_primitives
     plt.errorbar(
-        df2.index,
-        df2[("total_delta_ang", "mean")],
-        # yerr=df2[("runtime", "std")],
+        df2["num_primitives"],
+        df2[("num_targets", "mean")],
+        yerr=df2[("num_targets", "std")],
         fmt='o',
         capsize=5,
     )
-    plt.ylabel("Smoothness (seconds)")
-    plt.xlabel("Linear acceleration orientation resolution (degrees)")
+    plt.ylabel("Number of targets")
+    plt.xlabel("Log number of primitives")
     plt.show()
+    print(df2)
+    df2.to_csv("jawohl.csv")
     exit(0)
 
 if choose == LIN_MAGS:
